f5f3b89954764c3c3d761eab4995ade7dc03c8b4
Nick Stephens <nick.d.stephens@gmail.com>
2015-10-08 16:29:33 -0700

    Reorganize setup.py to compile list of data_files in build

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ AFL_UNIX_PATCH_FILE = os.path.join("patches", "afl-patch.diff")
 AFL_CGC_INSTALL_PATH = os.path.join("bin", "afl-cgc")
 SUPPORTED_ARCHES = ["aarch64", "x86_64", "i386", "arm", "ppc", "ppc64", "mips", "mips64"]
 
+data_files = [ ]
+
 # grab the afl-other-arch repo
 if not os.path.exists(AFL_UNIX_INSTALL_PATH):
     AFL_UNIX_REPO = "git@git.seclab.cs.ucsb.edu:shellphish/afl-other-arch.git"
@@ -27,6 +29,7 @@ if not os.path.exists(AFL_CGC_INSTALL_PATH):
         raise LibError("Unable to retrieve afl-cgc")
 
 def _build_all():
+    global data_files
 
     # build afls
     if subprocess.call(['./build.sh'] + SUPPORTED_ARCHES, cwd=AFL_UNIX_INSTALL_PATH) != 0:
@@ -42,42 +45,40 @@ def _build_all():
     if subprocess.call(["./fetchlibs.sh"], cwd=".") != 0:
         raise LibError("Unable to fetch libraries")
 
+    AFL_UNIX_FUZZ = os.path.join(AFL_UNIX_INSTALL_PATH)
+    AFL_CGC_FUZZ  = os.path.join(AFL_CGC_INSTALL_PATH)
+
+    # get data_files ready for exporting, probably a better way to do this
+    data_files.append((AFL_UNIX_FUZZ, (os.path.join(AFL_UNIX_FUZZ, "afl-fuzz"))))
+    data_files.append((AFL_CGC_FUZZ, (os.path.join(AFL_CGC_FUZZ, "afl-fuzz"))))
+
+    for ARCH in SUPPORTED_ARCHES:
+        TRACER_STR = os.path.join(AFL_UNIX_INSTALL_PATH, "tracers", ARCH)
+        data_files.append((TRACER_STR, (os.path.join(TRACER_STR, "afl-qemu-trace"),),))
+
+    # for each lib export it into 
+    for LIB in os.listdir("libs"):
+        OUTPUT_PATH = os.path.join("bin", "fuzzer-libs", LIB, "lib")
+        INPUT_DIR = os.path.join("libs", LIB, "lib")
+        for item in os.listdir(INPUT_DIR):
+            print item
+            # library directory transport everything
+            if os.path.isdir(os.path.join(INPUT_DIR, item)):
+                for library in os.listdir(os.path.join(INPUT_DIR, item)):
+                    data_files.append((os.path.join(INPUT_DIR, item, library), os.path.join(OUTPUT_PATH, item, library)))
+            else:
+                data_files.append((os.path.join(INPUT_DIR, item), os.path.join(OUTPUT_PATH, item)))
+                
+
+    # add cgc
+    TRACER_STR = os.path.join(AFL_CGC_INSTALL_PATH, "tracers", "i386")
+    data_files.append((TRACER_STR, (os.path.join(TRACER_STR, "afl-qemu-trace"),),))
+
 class build(_build):
     def run(self):
             self.execute(_build_all, (), msg="Building AFL and grabbing libraries")
             _build.run(self)
 cmdclass = {'build': build}
-
-AFL_UNIX_FUZZ = os.path.join(AFL_UNIX_INSTALL_PATH)
-AFL_CGC_FUZZ  = os.path.join(AFL_CGC_INSTALL_PATH)
-
-# get data_files ready for exporting, probably a better way to do this
-data_files = [
-    (AFL_UNIX_FUZZ, (os.path.join(AFL_UNIX_FUZZ, "afl-fuzz"),),),
-    (AFL_CGC_FUZZ, (os.path.join(AFL_CGC_FUZZ, "afl-fuzz"),),),
-    ]
-
-for ARCH in SUPPORTED_ARCHES:
-    TRACER_STR = os.path.join(AFL_UNIX_INSTALL_PATH, "tracers", ARCH)
-    data_files.append((TRACER_STR, (os.path.join(TRACER_STR, "afl-qemu-trace"),),))
-
-# for each lib export it into 
-for LIB in os.listdir("libs"):
-    OUTPUT_PATH = os.path.join("bin", "fuzzer-libs", LIB, "lib")
-    INPUT_DIR = os.path.join("libs", LIB, "lib")
-    for item in os.listdir(INPUT_DIR):
-        print item
-        # library directory transport everything
-        if os.path.isdir(os.path.join(INPUT_DIR, item)):
-            for library in os.listdir(os.path.join(INPUT_DIR, item)):
-                data_files.append((os.path.join(INPUT_DIR, item, library), os.path.join(OUTPUT_PATH, item, library)))
-        else:
-            data_files.append((os.path.join(INPUT_DIR, item), os.path.join(OUTPUT_PATH, item)))
-            
-
-# add cgc
-TRACER_STR = os.path.join(AFL_CGC_INSTALL_PATH, "tracers", "i386")
-data_files.append((TRACER_STR, (os.path.join(TRACER_STR, "afl-qemu-trace"),),))
 
 setup(
     name='fuzzer', version='1.0', description="Python wrapper for multiarch AFL",
