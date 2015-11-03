ddf1aa7fba3d8b0ea7cc0d883923fdc39baab5c4
Nick Stephens <nick.d.stephens@gmail.com>
2015-11-02 17:57:02 -0800

    Dictionary creation does not load libc

--- bin/create_dict.py
+++ bin/create_dict.py
@@ -24,32 +24,32 @@ def hexescape(s):
 
 def create(binary, outfile):
 
-        b = angr.Project(binary)
-        cfg = b.analyses.CFG(keep_input_state=True)
+    b = angr.Project(binary, load_options={'auto_load_libs': False})
+    cfg = b.analyses.CFG(keep_input_state=True)
 
-        string_references = [ ]
-        for f in cfg.function_manager.functions.values():
-            try:
-                string_references.append(f.string_references())
-            except ZeroDivisionError:
-                pass
+    string_references = [ ]
+    for f in cfg.function_manager.functions.values():
+        try:
+            string_references.append(f.string_references())
+        except ZeroDivisionError:
+            pass
 
-        string_references = sum(string_references, [])
+    string_references = sum(string_references, [])
 
-        strings = [] if len(string_references) == 0 else zip(*string_references)[1]
+    strings = [] if len(string_references) == 0 else zip(*string_references)[1]
 
-        valid_strings = filter(lambda s: len(s) <= 128 and len(s) > 0, strings)
-        if len(valid_strings) > 0:
-            with open(outfile, 'wb') as f:
-                for i, s in enumerate(valid_strings):
-                    # AFL has a limit of 128 bytes per dictionary entries
-                    if len(s) <= 128:
-                        esc_s = hexescape(s)
-                        f.write("string_%d=\"%s\"\n" % (i, esc_s))
+    valid_strings = filter(lambda s: len(s) <= 128 and len(s) > 0, strings)
+    if len(valid_strings) > 0:
+        with open(outfile, 'wb') as f:
+            for i, s in enumerate(valid_strings):
+                # AFL has a limit of 128 bytes per dictionary entries
+                if len(s) <= 128:
+                    esc_s = hexescape(s)
+                    f.write("string_%d=\"%s\"\n" % (i, esc_s))
 
-            return True
+        return True
 
-        return False
+    return False
 
 def main(argv):
 
