f55d16f3a7d2bfcefc08c660597219aace9f4acf
Nick Stephens <nick.d.stephens@gmail.com>
2016-01-23 16:00:14 -0800

    Remove found_crash check in crashes method

--- fuzzer/fuzzer.py
+++ fuzzer/fuzzer.py
@@ -253,9 +253,6 @@ class Fuzzer(object):
         :return: a list of strings which are crashing inputs
         '''
 
-        if not self.found_crash():
-            return [ ]
-
         crashes = set()
         for fuzzer in os.listdir(self.out_dir):
             crashes_dir = os.path.join(self.out_dir, fuzzer, "crashes")
