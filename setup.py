c0cf001646057dd5965bdb1f1e762de781de978f
Owlz <Owlz@users.noreply.github.com>
2016-08-22 01:02:25 -0400

    Tuple Bug :-)
    Python simplifies a single tuple into an object unless you give it an extra comma. This was a little bastard to find...

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from distutils.core import setup
 setup(
     name='fuzzer', version='1.0', description="Python wrapper for multiarch AFL",
     packages=['fuzzer', 'fuzzer.extensions'],
-    data_files = [ ("bin", (os.path.join("bin", "create_dict.py"))) ],
+    data_files = [ ("bin", (os.path.join("bin", "create_dict.py"),)) ],
     install_requires=['angr', 'shellphish-qemu', 'shellphish-afl']
 )
