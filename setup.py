989b94c61cb6d2c013e48e5c80ceffcc699e37e2
Yan <yans@yancomm.net>
2017-05-15 03:22:27 -0700

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from distutils.core import setup
 
 setup(
-    name='fuzzer', version='1.0', description="Python wrapper for multiarch AFL",
+    name='fuzzer', version='1.1', description="Python wrapper for multiarch AFL",
     packages=['fuzzer', 'fuzzer.extensions'],
     data_files = [ ("bin", (os.path.join("bin", "create_dict.py"),)) ],
     scripts = [ 'shellphuzz' ],
