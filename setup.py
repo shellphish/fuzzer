96870ae9e60df73ed813daeb873ae63c6f50f5c8
wgibbs15 <wfgibbs@asu.edu>
2019-04-30 17:08:11 -0400

    remove unused data_files

--- setup.py
+++ setup.py
@@ -4,6 +4,5 @@ from distutils.core import setup
 setup(
     name='phuzzer', version='1.1', description="Python wrapper for multiarch AFL",
     packages=['phuzzer', 'phuzzer.extensions'],
-    data_files = [ ("bin", (os.path.join("bin", "create_dict.py"),)) ],
     install_requires=['angr', 'shellphish-qemu', 'shellphish-afl', 'tqdm']
 )
