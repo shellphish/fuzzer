fa96feb565855b216f9d7ddf3b9949c3e877f061
wgibbs15 <wfgibbs@asu.edu>
2019-04-30 17:07:11 -0400

    Removed unused script

--- setup.py
+++ setup.py
@@ -5,6 +5,5 @@ setup(
     name='phuzzer', version='1.1', description="Python wrapper for multiarch AFL",
     packages=['phuzzer', 'phuzzer.extensions'],
     data_files = [ ("bin", (os.path.join("bin", "create_dict.py"),)) ],
-    scripts = [ 'shellphuzz' ],
     install_requires=['angr', 'shellphish-qemu', 'shellphish-afl', 'tqdm']
 )
